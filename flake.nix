530db89299551ced0aa596b8d16d61ca6aa32723
Tahlon Brahic <104690672+TahlonBrahic@users.noreply.github.com>
2024-10-28 18:46:10 -0500

    comment out unresolved flake outputs

--- flake.nix
+++ flake.nix
@@ -10,55 +10,38 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    systems = { 
+      url = "github:nix-systems/x86_64-linux";
+    };
   };
 
-  outputs = inputs@{ self, ... }:
+  outputs = inputs@{ self, systems, nixpkgs, ... }:
     let
-      # Systems that can run tests:
-      supportedSystems = [
-        "aarch64-linux"
-        "i686-linux"
-        "x86_64-linux"
-      ];
-
-      # Function to generate a set based on supported systems:
-      forAllSystems = inputs.nixpkgs.lib.genAttrs supportedSystems;
-
-      # Attribute set of nixpkgs for each system:
-      nixpkgsFor = forAllSystems (system:
-        import inputs.nixpkgs { inherit system; });
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
     in
-    {
-      homeManagerModules.sublimation = { ... }: {
-        imports = [ ./modules ];
-      };
-
-      #TODO Decide wheter or not to implement packages
-      packages = forAllSystems (system:
-        let pkgs = nixpkgsFor.${system}; in
-        {
-
-        });
-
-      #TODO Decide wheter or not to implement apps
-      apps = forAllSystems (system: {
-        #default = self.apps.${system}.;
-      });
-
-      checks = forAllSystems (system:
-        {
-          default = nixpkgsFor.${system}.callPackage ./test/basic.nix {
-            home-manager-module = inputs.home-manager.nixosModules.home-manager;
-            sublimation = self.homeManagerModules.sublimation;
-          };
-        });
-
-      devShells = forAllSystems (system: {
-        default = nixpkgsFor.${system}.mkShell {
-          buildInputs = with nixpkgsFor.${system}; [
-            #TODO add dependencies
-          ];
-        };
-      });
+  {
+    homeManagerModules.sublimation = { ... }: {
+      imports = [ ./modules ];
     };
-}
+
+    /*
+    packages = 
+
+    checks = {
+      default = nixpkgsFor.${system}.callPackage ./test/basic.nix {
+        home-manager-module = inputs.home-manager.nixosModules.home-manager;
+        sublimation = self.homeManagerModules.sublimation;
+      };
+    };
+
+    devShells = forAllSystems (system: {
+      default = nixpkgsFor.${system}.mkShell {
+      buildInputs = with nixpkgsFor.${system}; [
+            # TODO: add dependencies 
+        ];
+      };
+    });
+    */
+  };
+
