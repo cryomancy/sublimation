a02e3963837adfb385740e9801608f8c965568d1
TahlonBrahic <tahlonbrahic@gmail.com>
2025-01-22 06:40:02 -0600

    start sublimation pkg

---
+++ pkgs/sublimation/src/main.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+const Game = struct {
+    name: [_]u8,
+    appid: [_]u8
+};
+
+fn checkGamePresent(game: ) {
+
+}
+
+pub fn main() void {
+  var install = async checkGamePresent();
+}
